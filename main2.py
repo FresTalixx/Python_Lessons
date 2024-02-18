4ec71ba84e9463c22cb7830a4bc620a8a84c3e7d
frestalixx <frestalixx2@gmail.com>
2024-02-18 11:16:45 +0200

    Lesson2

--- main2.py
+++ main2.py
@@ -1,1 +1,25 @@
-print("2")
+class Student:
+    def __init__(self, grade, name):
+        self.name = name
+        self.grade = grade
+
+    def study(self, hours):
+        self.grade += hours
+
+    def play(self, hours):
+        self.grade -= hours * 0.05
+
+    def get_grade(self):
+        return self.grade
+
+
+student = Student(6, "Вася")
+lesson = float(input("Навчіть студента - "))
+student.study(lesson)
+
+game = float(input("Пограйте з студентом - "))
+student.play(game)
+
+grade = student.get_grade()
+
+print(f"Оцінка студента {student.name}: {grade}")
